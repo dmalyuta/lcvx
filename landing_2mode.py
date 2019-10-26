5063276625a9fc00722765123758d3cf1486a48c
Danylo Malyuta <danylo.malyuta@gmail.com>
2019-10-25 19:40:57 -0500

    Working example with "Martian divert" type landing, which is a lot more dynamic that the landing I had before

--- landing_2mode.py
+++ landing_2mode.py
@@ -32,18 +32,18 @@ class Lander(lcvx.Problem,object):
         
         # Physical parameters
         self.omega = 2*np.pi/(24*3600+39*60+35) # [rad/s] Mars spin
-        Tmax = 21500. # [N] Maximum thrust
         self.mass = 1700. # [kg] Lander mass
-        amax = Tmax/self.mass; # [m/s^2] Maximum acceleration
         self.g = 3.71 # [m/s^2] Mars surface gravity
         self.R = 3396.2e3 # [m] Mars radius
+        #Tmax = 21500. # [N] Maximum thrust
+        #amax = Tmax/self.mass; # [m/s^2] Maximum acceleration
         self.rho1 = [4,8]#[s_*amax for s_ in [0.2,0.6]] # [m/s^2] Smallest  acceleration
         self.rho2 = [8,12]#[s_*amax for s_ in [0.6,0.9]] # [m/s^2] Largest control acceleration
-        self.gs = np.deg2rad(85) # [rad] Smallest glideslope angle
+        self.gs = np.deg2rad(10) # [rad] Smallest glideslope angle
         
         # Boundary conditions
-        r0 = np.array([-100.,3000.])
-        v0 = np.array([-38.,-60.])
+        r0 = np.array([1500.,1200.])
+        v0 = np.array([50.,-70.])
         rf = np.array([0.,0.])
         vf = np.array([0.,0.])
         
@@ -79,7 +79,7 @@ class Lander(lcvx.Problem,object):
         self.tfmax = 100.
         
         # Optimization problem common parts
-        self.N = 30 # Temporal solution
+        self.N = 150 # Temporal solution
         x = [cvx.Parameter(nx)]+[self.Dx*cvx.Variable(nx) for _ in range(1,self.N+1)]
         xi = [cvx.Parameter()]+[cvx.Variable() for _ in range(1,self.N+1)]
         u = [[self.Du[i]*cvx.Variable(nu) for __ in range(self.N)] for i in range(self.M)]
@@ -274,7 +274,7 @@ def make_arc(angle,magnitude,direction):
     return arc
 
 def draw_polar_grid(ax,angle,bounds,direction,angle_ticks,magnitude_ticks,
-                    border_style,grid_style,labels=True):
+                    border_style,grid_style,labels=True,angle_tick_blacklist=None):
     """
     Draw a polar grid.
     
@@ -290,33 +290,39 @@ def draw_polar_grid(ax,angle,bounds,direction,angle_ticks,magnitude_ticks,
         Polar plot "nominal" (zero angle) direction.
     """
     eps = np.finfo(np.float64).eps
-    text_style = dict(va='top')
+    ang_text_style = dict(va='bottom',ha='center',fontsize=10)
+    mag_text_style = dict(va='center',ha='right',fontsize=10)
+    mag_offset = 0.5
+    ang_offset = 0.4
     # Draw the polar plot border
     arc1 = make_arc(angle,bounds[0],direction)
     arc2 = np.fliplr(make_arc(angle,bounds[1],direction))
     arc = np.column_stack([arc1,arc2,arc1[:,0]])
     ax.plot(arc[0],arc[1],**border_style)
     if labels:
-        ax.text(arc2[0][0],arc2[1][0],'$%d^\circ$'%(angle/2),va='top')
-        ax.text(arc2[0][-1],arc2[1][-1],
-                '$%d^\circ$'%(-angle/2),**text_style)
+        arc_ = make_arc(angle,bounds[1]+mag_offset,direction)
+        ax.text(arc_[0][-1],arc_[1][-1],'$%d^\circ$'%(angle/2),**ang_text_style)
+        ax.text(arc_[0][0],arc_[1][0],'$%d^\circ$'%(-angle/2),**ang_text_style)
     # Draw the polar plot grid
     for ang in angle_ticks:
         if np.abs(ang)<angle/2-eps:
             grid_line = np.column_stack([bnd*tools.R2d(np.deg2rad(ang)).dot(direction) for bnd in bounds])
             ax.plot(grid_line[0],grid_line[1],**grid_style)
-            if labels:
-                ax.text(grid_line[0][-1],grid_line[1][-1],'$%d^\circ$'%(ang),**text_style)
+            if labels and not angle_tick_blacklist(ang):
+                pos_ = np.column_stack([(bnd+mag_offset)*tools.R2d(np.deg2rad(ang)).dot(direction) for bnd in bounds])
+                ax.text(pos_[0][-1],pos_[1][-1],'$%d^\circ$'%(ang),**ang_text_style)
     for mag in magnitude_ticks:
-        if mag>bounds[0]+eps and mag<bounds[1]-eps:
+        if mag>=bounds[0] and mag<=bounds[1]:
             grid_line = make_arc(angle,mag,direction)
-            ax.plot(grid_line[0],grid_line[1],**grid_style)
+            if mag>bounds[0]+eps and mag<bounds[1]-eps:
+                ax.plot(grid_line[0],grid_line[1],**grid_style)
             if labels:
-                ax.text(grid_line[0][-1],grid_line[1][-1],'$%d$~m/s$^2$'%(mag),**text_style)
+                pos_ = make_arc(angle+np.rad2deg(ang_offset/mag)*2,mag,direction)
+                ax.text(pos_[0][-1],pos_[1][-1],'$%d$~m/s$^2$'%(mag),**mag_text_style)
 
 primal = post_process(primal)
 
-save_pdf = False # True to save figure PDFs
+save_pdf = True # True to save figure PDFs
 
 K = rocket.K
 M = rocket.M
@@ -330,9 +336,9 @@ colors = ['green','blue','red','cyan','magenta']
 
 #### Trajectory plot
 
-pos_style = dict(color='black',linewidth=0.5,zorder=90)
+pos_style = dict(color='black',linewidth=1,zorder=90)
 vel_style = dict(color='red',linewidth=0.5)
-thrust_style = [dict(color=colors[i]) for i in range(M)]
+thrust_style = [dict(color=colors[i],linewidth=0.5) for i in range(M)]
 ground_style = dict(linewidth=0,color='black',alpha=0.2,zorder=1)
 gs_style = dict(color='black',linestyle='--',linewidth=0.5,zorder=1)
 v_whisker_scale = 5
@@ -343,7 +349,7 @@ matplotlib.rcParams.update({'font.size': 18})
 n_gs = [tools.R2d(-(np.pi/2-gs)).dot(np.array([0,1])),
         tools.R2d(np.pi/2-gs).dot(np.array([0,1]))]
 
-fig = plt.figure(1,figsize=(4.5,8.5))
+fig = plt.figure(1,figsize=(9,4))
 plt.clf()
 ax = fig.add_subplot(111)
 ax.axis('equal')
@@ -369,87 +375,94 @@ for i in range(2):
 ax.set_xlim(xlims)
 ax.set_ylim(ylims)
 
+if save_pdf:
+    fig.savefig('figures/rocket_trajectory.pdf',bbox_inches='tight',format='pdf',transparent=True)
+
 #### Thrust polar plot
 
 border_style = [dict(color=colors[i],linewidth=1.5) for i in range(M)]
 grid_style = [dict(color=colors[i],linewidth=0.5,alpha=0.3) for i in range(M)]
-thrust_style = [dict(color=colors[i],marker='s',markersize=5,linestyle='none',zorder=90) for i in range(M)]
+thrust_style = [dict(color=colors[i],marker='x',markersize=5,linestyle='none',zorder=90) for i in range(M)]
 origin_style = dict(color='black',linestyle='none',marker='.',markersize=5,zorder=100)
 angles = np.linspace(-90.,90.,np.int(180./20.+1))
 magnitudes = np.linspace(0,np.ceil(rho2[-1]),np.int(np.ceil(rho2[-1])/1.+1))
 thrust_direction = np.array([0,-1])
 
-if save_pdf:
-    fig.savefig('figures/rocket_trajectory.pdf',bbox_inches='tight',format='pdf',transparent=True)
-
 matplotlib.rcParams.update({'font.size': 14})
 
-fig = plt.figure(2,figsize=(4,5))
-plt.clf()
-ax = fig.add_subplot(111)
-ax.axis('equal')
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-plt.tight_layout()
-plt.axis('off')
-ax.plot(0,0,**origin_style)
-for i in range(M):
-    draw_polar_grid(ax,theta[i],[rho1[i],rho2[i]],thrust_direction,angles,
-                    magnitudes,border_style[i],grid_style[i])
-    ax.plot([-u if la.norm(u)>np.sqrt(1e-5) else np.nan for u in primal['u'][i][0]],
-            [-u if la.norm(u)>np.sqrt(1e-5) else np.nan for u in primal['u'][i][1]],**thrust_style[i])
-    
-if save_pdf:
-    fig.savefig('figures/rocket_input_polar.pdf',bbox_inches='tight',format='pdf',transparent=True)
+# =============================================================================
+# fig = plt.figure(2,figsize=(4,5))
+# plt.clf()
+# ax = fig.add_subplot(111)
+# ax.axis('equal')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+# plt.tight_layout()
+# plt.axis('off')
+# ax.plot(0,0,**origin_style)
+# for i in range(M):
+#     draw_polar_grid(ax,theta[i],[rho1[i],rho2[i]],thrust_direction,angles,
+#                     magnitudes,border_style[i],grid_style[i])
+#     ax.plot([-u if la.norm(u)>np.sqrt(1e-5) else np.nan for u in primal['u'][i][0]],
+#             [-u if la.norm(u)>np.sqrt(1e-5) else np.nan for u in primal['u'][i][1]],**thrust_style[i])
+#     
+# if save_pdf:
+#     fig.savefig('figures/rocket_input_polar.pdf',bbox_inches='tight',format='pdf',transparent=True)
+# =============================================================================
 
 #### Thrust magnitude plot
 
-u_style = dict(edgecolor='black',color='black',alpha=0.3,step='post',zorder=90)
+u_style = dict(edgecolor='black',color='black',alpha=0.3,step='post',linewidth=0,zorder=90)
 sigma_style = dict(linestyle='none',color='blue',marker='.',markersize=2,zorder=91)
 u_scale = 1
 
 fig = plt.figure(3,figsize=[7.5,5])
 plt.clf()
 ax = fig.add_subplot(211)
-sum_u_total = sum([la.norm(primal['u'][i]*u_scale,axis=0) for i in range(len(primal['u']))])
-sum_sigma = sum(primal['sigma'])*u_scale
-ax.fill_between(t[:-1],sum_u_total,**u_style)
-ax.plot(t[:-1],sum_sigma,**sigma_style)
+u_ = [np.column_stack([primal['u'][i],primal['u'][i][:,-1]]) for i in range(M)]
+sigma_ = [np.append(primal['sigma'][i],primal['sigma'][i][-1]) for i in range(M)]
+sum_u_total = sum([la.norm(u_[i]*u_scale,axis=0) for i in range(len(u_))])
+sum_sigma = sum(sigma_)*u_scale
+ax.fill_between(t/t[-1],sum_u_total,**u_style)
+ax.plot(t/t[-1],sum_sigma,**sigma_style)
 for i in range(M):
     ax.axhline(rho2[i]*u_scale,linewidth=1,linestyle='--',color='gray')
 ax.autoscale(tight=True)
-ax.set_xlabel('Time [s]')
-ax.set_ylabel('$\sum_{i=1}^M\|u_i(t)\|_2$')
+ax.set_xlabel('Normalized time $\hat t$')
+ax.set_ylabel('$\sum_{i=1}^M\|u_i(t)\|_2$ [m/s$^2$]')
 ax.set_yticks(ticks=np.concatenate([[0],[rho2[i]*u_scale for i in range(M)]]))
-ax.set_xticks(ticks=np.linspace(0,t[-2],5))
-ax.set_xlim([0,t[-2]])
+ax.set_xticks(ticks=np.linspace(0,1,5))
+ax.set_xlim([0,1])
 for i in range(M):
     ax = fig.add_subplot(2,M,M+1+i)
-    ax.fill_between(t[:-1],la.norm(primal['u'][i],axis=0)*u_scale,**u_style)
-    ax.plot(t[:-1],primal['sigma'][i]*u_scale,**sigma_style)
+    ax.fill_between(t/t[-1],la.norm(u_[i],axis=0)*u_scale,**u_style)
+    ax.plot(t/t[-1],sigma_[i]*u_scale,**sigma_style)
     ax.axhline(rho2[i]*u_scale,linewidth=1,linestyle='--',color='gray')
     ax.axhline(rho1[i]*u_scale,linewidth=1,linestyle='--',color='gray')
     ax.autoscale(tight=True)
-    ax.set_xlabel('Time [s]')
-    ax.set_ylabel('$\|u_{%d}(t)\|_2$'%(i+1))
+    ax.set_xlabel('Normalized time $\hat t$')
+    ax.set_ylabel('$\|u_{%d}(t)\|_2$ [m/s$^2$]'%(i+1))
     ax.set_yticks(ticks=np.linspace(0,rho2[i],5))
-    ax.set_xticks(ticks=np.linspace(0,t[-2],3))
-    ax.set_xlim([0,t[-2]])
-plt.tight_layout(pad=0.5,h_pad=0,w_pad=0)
+    ax.set_xticks(ticks=np.linspace(0,1,5))
+    ax.set_xlim([0,1])
+plt.tight_layout(pad=0.5,h_pad=0,w_pad=0.5)
 
 if save_pdf:
     fig.savefig('figures/rocket_input_magnitude.pdf',bbox_inches='tight',format='pdf',transparent=True)
     
-#### Input valuability plot (support functions)
-    
-#%%
+#### Input valuation plot (support functions)
 
 cost_coeff = [-(dual['beta_2_%d'%(i+1)]-dual['beta_3_%d'%(i+1)]).flatten() for i in range(M)]
 cost_coeff /= np.max(np.abs(cost_coeff))
-input_active = [la.norm(primal['u'][i],axis=0)>1e-3 for i in range(M)]
+input_active_tol = 1e-1
+input_active = [la.norm(primal['u'][i],axis=0)>input_active_tol for i in range(M)]
 y = misc['y']
-valuation = [[tools.support(y[k],C[i])*rho2[i] for k in range(y.shape[0])] for i in range(M)]
-valuation /= np.max(np.abs(valuation)) # normalize
+beta2 = [dual['beta_2_%d'%(i+1)].flatten() for i in range(M)]
+beta3 = [dual['beta_3_%d'%(i+1)].flatten() for i in range(M)]
+valuation = [[tools.support(y[k],C[i])*rho2[i] if not
+              la.norm(primal['u'][i][:,k])<input_active_tol else np.nan for k in
+              range(y.shape[0])] for i in range(M)]
+valuation /= np.nanmax(np.abs(valuation)) # normalize
 r_ = rho2[1]/rho2[0]
 a_ = np.cos(np.deg2rad(theta[0]-theta[1])/2)
 b_ = np.sin(np.deg2rad(theta[0]-theta[1])/2)
@@ -464,36 +477,48 @@ fig = plt.figure(4,figsize=(6.4,4.8))
 plt.clf()
 ax = fig.add_subplot(111)
 for i in range(M):
-    ax.plot(t[:-1],valuation[i],color=colors[i],label='$\hat v_{%d}(t)$'%(i+1),
-            **dual_style)
-    ax.fill_between(t[:-1],input_active[i],color=colors[i],**u_active_style)
-ax.set_xlabel('Time $t$ [s]')
+    ax.plot(t[:-1]/t[-1],valuation[i],color=colors[i],linestyle='none',**dual_style)
+    ax.step(t[:-1]/t[-1],valuation[i],color=colors[i],label='$\hat v_{%d}(t)$'%(i+1),
+            where='post')
+    ax.fill_between(t[:-1]/t[-1],input_active[i],color=colors[i],**u_active_style)
+ax.set_xlabel('Normalized time $\hat t$')
 ax.set_ylabel('Normalized valuation $\hat v_i(t)$')
 ax.legend()
 plt.tight_layout()
 ax.autoscale(tight=True)
 
-fig = plt.figure(5,figsize=(6.4,4.8))
-plt.clf()
-ax = fig.add_subplot(111)
-for i in range(M):
-    ax.plot(t[:-1],cost_coeff[i],color=colors[i],
-            label='$\hat\\beta_2^{%d}(t)-\hat\\beta_3^{%d}(t)$'%(i+1,i+1),**dual_style)
-ax.set_xlabel('Time $t$ [s]')
-ax.set_ylabel('Normalized $\hat\\beta_2^i(t)-\hat\\beta_3^i(t)$')
-ax.legend()
-plt.tight_layout()
-ax.autoscale(tight=True)
-ylims = ax.get_ylim()
-for i in range(M):
-    ax.fill_between(t[:-1],input_active[i]*ylims[0],input_active[i]*ylims[1],
-                    color=colors[i],**u_active_style)
-ax.axhline(0,color='black',linestyle='--',zorder=0)
+if save_pdf:
+    fig.savefig('figures/rocket_input_valuation.pdf',bbox_inches='tight',format='pdf',transparent=True)
+    
+#### Dual variable plots (support functions)
+
+# =============================================================================
+# fig = plt.figure(5,figsize=(6.4,4.8))
+# plt.clf()
+# ax = fig.add_subplot(111)
+# for i in range(M):
+#     ax.plot(t[:-1],cost_coeff[i],color=colors[i],linestyle='none',**dual_style)
+#     ax.step(t[:-1],cost_coeff[i],color=colors[i],
+#             label='$\hat\\beta_2^{%d}(t)-\hat\\beta_3^{%d}(t)$'%(i+1,i+1),where='post')
+# ax.set_xlabel('Time $t$ [s]')
+# ax.set_ylabel('Normalized $\hat\\beta_2^i(t)-\hat\\beta_3^i(t)$')
+# ax.legend()
+# plt.tight_layout()
+# ax.autoscale(tight=True)
+# ylims = ax.get_ylim()
+# for i in range(M):
+#     ax.fill_between(t[:-1],input_active[i]*ylims[0],input_active[i]*ylims[1],
+#                     color=colors[i],**u_active_style)
+# ax.axhline(0,color='black',linestyle='--',zorder=0)
+# 
+# if save_pdf:
+#     fig.savefig('figures/rocket_argmax_beta_diff.pdf',bbox_inches='tight',format='pdf',transparent=True)
+# =============================================================================
 
 primer_style = dict(color='black')
 primer_style_ic = dict(color='black',marker='x',linestyle='none')
 equiv_plane_style = gs_style
-yscale = 1e-3
+yscale = rho2[0]/np.max(la.norm(y,axis=1))
 
 fig = plt.figure(6,figsize=(6.4,4.8))
 plt.clf()
@@ -501,24 +526,30 @@ ax = fig.add_subplot(111)
 ax.axis('equal')
 for i in range(M):
     draw_polar_grid(ax,theta[i],[rho1[i],rho2[i]],np.array([0,1]),
-                    angles,magnitudes,
-                    border_style[i],grid_style[i],labels=False)
+                    angles,np.array(range(rho1[i]+i,rho2[i]+1)),
+                    border_style[i],grid_style[i],
+                    angle_tick_blacklist=lambda ang:ang<=theta[1]/2 and ang>=-theta[1]/2)
     ax.plot([u if la.norm(u)>np.sqrt(1e-5) else np.nan for u in primal['u'][i][0]],
             [u if la.norm(u)>np.sqrt(1e-5) else np.nan for u in primal['u'][i][1]],
             label='$u_{%d}(t)$'%(i),**thrust_style[i])
 ax.plot(0,0,**origin_style)
-ax.plot(yscale*y[:,0],yscale*y[:,1],label='$y(t)$',**primer_style)
-ax.plot(yscale*y[0,0],yscale*y[0,1],label='$y(0)$',**primer_style_ic)
+ax.plot(yscale*y[:,0],yscale*y[:,1],label='$\hat y(t)$',**primer_style)
+ax.plot(yscale*y[0,0],yscale*y[0,1],label='$\hat y(0)$',**primer_style_ic)
 ax.set_xlabel('$x$-acceleration $u_{i,1}(t)$ [m/s$^2$]')
 ax.set_ylabel('$y$-acceleration $u_{i,2}(t)$ [m/s$^2$]')
 plt.tight_layout()
 #ax.autoscale(tight=True)
-fig.canvas.draw()
 ylims = ax.get_ylim()
+ylims = [ylims[0],13]
+ax.set_ylim(ylims)
+fig.canvas.draw()
 xlims = ax.get_xlim()
-s = xlims[1]/n_equiv[0]
+s = xlims[1]*2/n_equiv[0]
 ax.plot([0,s*n_equiv[0]],[0,s*n_equiv[1]],**equiv_plane_style)
 ax.plot([0,-s*n_equiv[0]],[0,s*n_equiv[1]],**equiv_plane_style)
 ax.set_xlim(xlims)
 ax.set_ylim(ylims)
 ax.legend()
+
+if save_pdf:
+    fig.savefig('figures/rocket_primer_vector.pdf',bbox_inches='tight',format='pdf',transparent=True)
